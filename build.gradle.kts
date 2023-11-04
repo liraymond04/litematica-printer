619318c50ede424d72b7fe844d24020cb40aadf2
liraymond04 <li.raymond04@gmail.com>
2023-11-04 15:14:27 -0700

    Add implementation for 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,10 @@ val buildAll = tasks.create("buildAll") {
     dependsOn(":implementation:v1_17:build")
     dependsOn(":implementation:v1_18:build")
     dependsOn(":implementation:v1_19:build")
+    dependsOn(":implementation:v1_20_1:build")
     doLast {
         println("Copying files...")
+        file("implementation/v1_20_1/build/libs/v1_20_1.jar").copyTo(file("build/${archives_base_name}-1.20.1-${mod_version}.jar"), true)
         file("implementation/v1_19/build/libs/v1_19.jar").copyTo(file("build/${archives_base_name}-1.19-${mod_version}.jar"), true)
         file("implementation/v1_18/build/libs/v1_18.jar").copyTo(file("build/${archives_base_name}-1.18-${mod_version}.jar"), true)
         file("implementation/v1_17/build/libs/v1_17.jar").copyTo(file("build/${archives_base_name}-1.17-${mod_version}.jar"), true)
